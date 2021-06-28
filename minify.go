ed079b849893d41372abe7256fe9253c22e72db2
mzyl <maxzylstra@gmail.com>
2021-06-28 18:15:56 -0500

    no longer need this, thanks ikiris.

--- minify.go
+++ minify.go
@@ -2,8 +2,10 @@ package main
 
 import (
     "bufio"
+    "fmt"
     "log"
     "os"
+    "strings"
 
     "github.com/mzyl/bookbytes/bookbytes"
 )
@@ -14,11 +16,11 @@ func main() {
     // (book.fulltext might actually be a minified version already)
     // write book.fulltext to ../minified/book.filename
     var line string
-    var filename string
+    var filepath string
 //    var minified int
     var count int
 
-    file, err := os.Open("booklist.txt")
+    file, err := os.Open("doclist.txt")
     if err != nil {
         log.Fatal(err)
     }
@@ -27,25 +29,44 @@ func main() {
 
     for booklist.Scan() {
         line = booklist.Text()
-        filename = "../library/htmlmirror/" + line[2:]
-        book := bookbytes.GenerateBook(filename, 0)
-        
+        //filepath = "../library/htmlmirror/" + line[2:]
+        filepath = "docs/" + line[2:]
+        fmt.Println(filepath)
+        book := bookbytes.GenerateBook(filepath, 0)
+
+        newpath := "../library/minified/" + line[2:]
+        pathsplit := strings.Split(newpath, "/")
+        path := strings.Join(pathsplit[:len(pathsplit)-1], "/")
+        fmt.Println()
+        filename := pathsplit[len(pathsplit)-1]
+        fmt.Println(filename)
+        filenameslice := append([]string{"/"}, filename)
+        fmt.Println("filename:", filenameslice)
+        fmt.Println("path: ", path)
+        fmt.Println(pathsplit[len(pathsplit)-1])
+
+        os.MkdirAll(path, 0770)
+
         println("before")
-        f, err := os.Create("../library/minified/" + line[2:])
+        f, err := os.Create(newpath)
         if err != nil {
+            fmt.Println(err) 
             log.Fatal(err)
         }
         defer f.Close()
         println("after")
+
+        stripped := strings.TrimSpace(strings.Join(bookbytes.GetFullHtml(book), ""))
         
         w := bufio.NewWriter(f)
-        _, err = f.WriteString(bookbytes.GetFulltext(book))
+        //_, err = f.WriteString(bookbytes.GetFulltext(book))
+        _, err = f.WriteString(stripped)
         if err != nil {
             log.Fatal(err)
         }
         w.Flush()
 
-        println(filename)
+        println(filepath)
         bookbytes.BookPrinter(book)
         count++
         break
